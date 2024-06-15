4b4cba203d602b0218f1297c73756caa9b8e718c
zakashour <zak.ashour@trucesoftware.com>
2024-06-15 07:48:29 -0500

    Initial Commit

---
+++ dicee_swiftui/dicee_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_swiftuiApp.swift
+//  dicee_swiftui
+//
+//  Created by Zak Ashour on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct dicee_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
